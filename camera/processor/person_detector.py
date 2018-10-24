c3bee4d734aeb2b0b5088b3157da11758169b7eb
hanzo <kazu.queserasera@gmail.com>
2018-10-24 19:55:09 +0900

    Update person_detector.py

--- camera/processor/person_detector.py
+++ camera/processor/person_detector.py
@@ -1,5 +1,6 @@
 from __future__ import print_function
 from imutils.video.pivideostream import PiVideoStream
+from imutils.object_detection import non_max_suppression
 import imutils
 import time
 import numpy as np
